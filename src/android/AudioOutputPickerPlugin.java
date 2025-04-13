7dc4f808db157f9a69b6e6a8611a0c16ac2d4d5a
mlerena <manuellerena@gmail.com>
2025-04-13 15:53:32 -0300

    Add event to detect current audio output

--- src/android/AudioOutputPickerPlugin.java
+++ src/android/AudioOutputPickerPlugin.java
@@ -1,45 +1,96 @@
 package com.kuackmedia.plugins.audiooutputpicker;
 
-import androidx.mediarouter.app.MediaRouteChooserDialog;
-import androidx.mediarouter.media.MediaControlIntent;
-import androidx.mediarouter.media.MediaRouteSelector;
-import androidx.mediarouter.media.MediaRouter;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.AudioDeviceInfo;
+import android.media.AudioManager;
+import android.os.Build;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 public class AudioOutputPickerPlugin extends CordovaPlugin {
 
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if ("showAudioOutputPicker".equals(action)) {
-            showAudioOutputPicker(callbackContext);
-            return true;
+    private CallbackContext eventCallbackContext = null;
+    private final BroadcastReceiver audioReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (eventCallbackContext != null) {
+                JSONObject deviceInfo = getAudioOutputInfo();
+                PluginResult result = new PluginResult(PluginResult.Status.OK, deviceInfo);
+                result.setKeepCallback(true);
+                eventCallbackContext.sendPluginResult(result);
+            }
         }
-        callbackContext.error("Invalid action");
-        return false;
+    };
+
+    @Override
+    public void pluginInitialize() {
+        IntentFilter filter = new IntentFilter(AudioManager.ACTION_AUDIO_BECOMING_NOISY);
+        filter.addAction(BluetoothAdapter.ACTION_CONNECTION_STATE_CHANGED);
+        filter.addAction(AudioManager.ACTION_HEADSET_PLUG);
+        cordova.getActivity().registerReceiver(audioReceiver, filter);
     }
 
-    private void showAudioOutputPicker(final CallbackContext callbackContext) {
-        cordova.getActivity().runOnUiThread(() -> {
-            try {
-                AppCompatActivity activity = (AppCompatActivity) cordova.getActivity();
+    @Override
+    public void onDestroy() {
+        cordova.getActivity().unregisterReceiver(audioReceiver);
+        super.onDestroy();
+    }
 
-                MediaRouter mediaRouter = MediaRouter.getInstance(activity);
-                MediaRouteSelector selector = new MediaRouteSelector.Builder()
-                        .addControlCategory(MediaControlIntent.CATEGORY_LIVE_AUDIO)
-                        .build();
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        switch (action) {
+            case "getCurrentAudioOutput":
+                JSONObject deviceInfo = getAudioOutputInfo();
+                callbackContext.success(deviceInfo);
+                return true;
+            case "startAudioOutputListener":
+                this.eventCallbackContext = callbackContext;
+                PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
+                pluginResult.setKeepCallback(true);
+                callbackContext.sendPluginResult(pluginResult);
+                return true;
+            case "stopAudioOutputListener":
+                this.eventCallbackContext = null;
+                callbackContext.success("Listener detenido");
+                return true;
+            default:
+                callbackContext.error("Invalid action");
+                return false;
+        }
+    }
 
-                MediaRouteChooserDialog chooserDialog = new MediaRouteChooserDialog(activity);
-                chooserDialog.setRouteSelector(selector);
-                chooserDialog.show();
-
-                callbackContext.success("Audio Output Picker abierto");
-            } catch (Exception e) {
-                callbackContext.error("Error al abrir el diálogo: " + e.getMessage());
+    private JSONObject getAudioOutputInfo() {
+        AudioManager audioManager = (AudioManager) cordova.getActivity().getSystemService(Context.AUDIO_SERVICE);
+        JSONObject result = new JSONObject();
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                AudioDeviceInfo[] devices = audioManager.getDevices(AudioManager.GET_DEVICES_OUTPUTS);
+                for (AudioDeviceInfo deviceInfo : devices) {
+                    if (deviceInfo.isSink()) {
+                        int type = deviceInfo.getType();
+                        if (type == AudioDeviceInfo.TYPE_BLUETOOTH_A2DP || type == AudioDeviceInfo.TYPE_BLUETOOTH_SCO) {
+                            result.put("type", "bluetooth");
+                            result.put("name", deviceInfo.getProductName());
+                            return result;
+                        } else if (type == AudioDeviceInfo.TYPE_WIRED_HEADPHONES || type == AudioDeviceInfo.TYPE_WIRED_HEADSET) {
+                            result.put("type", "wired_headset");
+                            result.put("name", "Auriculares cableados");
+                            return result;
+                        }
+                    }
+                }
             }
-        });
+            // Default
+            result.put("type", "speaker");
+            result.put("name", "Altavoz del teléfono");
+        } catch (JSONException e) {
+            // Manejo de excepción
+        }
+        return result;
     }
 }
--- www/AudioOutputPicker.js
+++ www/AudioOutputPicker.js
@@ -1,8 +1,14 @@
 var exec = require('cordova/exec');
 
 var AudioOutputPicker = {
-  showAudioOutputPicker: function(success, error) {
-    exec(success, error, 'AudioOutputPicker', 'showAudioOutputPicker', []);
+  getCurrentAudioOutput: function(success, error) {
+    exec(success, error, 'AudioOutputPicker', 'getCurrentAudioOutput', []);
+  },
+  startAudioOutputListener: function(callback, error) {
+    exec(callback, error, 'AudioOutputPicker', 'startAudioOutputListener', []);
+  },
+  stopAudioOutputListener: function(success, error) {
+    exec(success, error, 'AudioOutputPicker', 'stopAudioOutputListener', []);
   }
 };
 
